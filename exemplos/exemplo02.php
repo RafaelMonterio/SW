80ee6e37fbea7324c65826254e875d1588c2fff9
Rafael Monteiro <161628330+RafaelMonterio@users.noreply.github.com>
2025-03-07 17:01:11 -0300

    Delete exemplos directory

--- exemplos/exemplo02.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $a = 4;
-    $b=  "101 Dálmatas";
-    $s = $a + $b;
-
-    echo $s;
-?>
--- exemplos/exemplo03.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    $a = "oi";
-    $b = "2F";
-
-    echo $a." ".$b." boa tarde!";
-
-?>
