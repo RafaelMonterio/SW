487108150147eeb942047db034f53b4b50ee812f
Rafael Monteiro <161628330+RafaelMonterio@users.noreply.github.com>
2025-08-15 17:13:34 -0300

    Add files via upload

---
+++ Loja1/form_cadastrar.php
@@ -0,0 +1,26 @@
+<?php
+    include "cabecalho.php";
+?>
+
+<body>
+    <div class="container">
+        <h1>Cadatro</h1>
+        <br>
+         <form>
+            <div class="mb-3">
+                <input type="text" name="produto" class="form-control"  placeholder="Digite o nome do Produto">
+            </div>
+            <div class="mb-3">
+                <input type="text" class="form-control" placeholder="Digite o preço de Produto">
+            </div>
+            <div class="mb-3">
+                <input type="text" class="form-control" placeholder="Digite quantidade ">
+            </div>
+            <button type="submit" class="btn btn-primary">Cadastrar</button>
+
+
+            
+        </form>
+    </div>
+</body>
+</html>
---
+++ Loja1/listar.php
@@ -0,0 +1,42 @@
+<?php
+    include "cabecalho.php";
+?>
+
+<body>
+        <h1>Cadatro</h1>
+        <br>
+    <div class="container">
+        <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>John</td>
+      <td>Doe</td>
+      <td>@social</td>
+    </tr>
+  </tbody>
+</table>
+    </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script> 
+</body>
+</html>
